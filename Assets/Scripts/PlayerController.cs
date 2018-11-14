877d38336e788c8273fe030938ea3504bdf94346
Khuong Tiet <khuongtiet@dhcp-v047-203.mobile.uci.edu>
2018-11-13 17:20:02 -0800

    first commit for collab

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    private const float Y_ANGLE_MIN = 0.0f;
+    private const float Y_ANGLE_MAX = 50.0f;
+    private float currentX = 0.0f;
+    private float currentY = 0.0f;
+    public float moveSpeed;
+    public float turnSpeed = 150f;
+    private CharacterController characterController;
+
+    private Vector3 moveInput;
+    private Vector3 moveVelocity;
+
+    private Camera mainCamera;
+
+	// Use this for initialization
+	void Start () {
+        characterController = GetComponent<CharacterController>();
+        mainCamera = FindObjectOfType<Camera>();
+
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+        var horizontal = Input.GetAxisRaw("Horizontal");
+        var vertical = Input.GetAxisRaw("Vertical");
+
+        var movement = new Vector3(horizontal, 0, vertical);
+
+        transform.Rotate(Vector3.up, horizontal * turnSpeed * Time.deltaTime);
+
+        if (vertical != 0.0f) {
+            characterController.SimpleMove(transform.forward * moveSpeed * vertical);
+        }
+
+        //characterController.SimpleMove(movement * Time.deltaTime * moveSpeed);
+
+        //if (movement.magnitude > 0) {
+        //    Quaternion newDirection = Quaternion.LookRotation(movement);
+
+        //    transform.rotation = Quaternion.Slerp(transform.rotation, newDirection, Time.deltaTime * turnSpeed);
+        //} 
+
+    }
+}
---
+++ Assets/Scripts/ThirdPersonCamera.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCamera : MonoBehaviour {
+    private const float Y_ANGLE_MIN = 0.0f;
+    private const float Y_ANGLE_MAX = 50.0f;
+
+    private const float X_ANGLE_MIN = -90.0f;
+    private const float X_ANGLE_MAX = 90.0f;
+
+    public Transform lookAt;
+    public Transform camTransform;
+    public float distance = 7.0f;
+
+    private Camera cam;
+    private float currentX = 0.0f;
+    private float currentY = 0.0f;
+    private float sensitivityX = 4.0f;
+    private float sensitiviiyY = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+        camTransform = transform;
+        cam = Camera.main;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        currentX += Input.GetAxis("Mouse X");
+        currentY += Input.GetAxis("Mouse Y");
+
+        currentY = Mathf.Clamp(currentY, Y_ANGLE_MIN, Y_ANGLE_MAX);
+        currentX = Mathf.Clamp(currentX, X_ANGLE_MIN, X_ANGLE_MAX);
+
+
+
+	}
+
+    private void LateUpdate() {
+        Vector3 dir = new Vector3(4, 0, -distance);
+        Quaternion rotation = Quaternion.Euler(30, 0, 0);
+        camTransform.position = lookAt.position + rotation * dir;
+        camTransform.LookAt(lookAt.position);
+    }
+}
