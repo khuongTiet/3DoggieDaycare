11a90413b8680ca742579a4eec7b8ea5bdefbc91
Khuong Tiet <khuongtiet@dhcp-v024-255.mobile.uci.edu>
2018-11-19 16:32:19 -0800

    added beagle and detection range

---
+++ Assets/DogMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DogMovement : MonoBehaviour {
+    Transform player;
+    //PlayerHealth playerHealth;
+    //EnemyHealth enemyHealth;
+    UnityEngine.AI.NavMeshAgent nav;
+
+    public float DetectRange;
+    public bool FollowingPlayer = false;
+
+    void Start()
+    {
+        nav = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        nav.Warp(transform.position);
+    }
+
+
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        //playerHealth = player.GetComponent <PlayerHealth> ();
+        //enemyHealth = GetComponent <EnemyHealth> ();
+    }
+
+
+    void Update()
+    {
+        //if(enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0)
+        //{
+        if (Vector3.Distance(player.position, transform.position) <= DetectRange) {
+            FollowingPlayer = true;
+        } else {
+            FollowingPlayer = false;
+        }
+
+        if (FollowingPlayer) {
+            nav.SetDestination(player.position);
+        }
+        //}
+        //else
+        //{
+        //    nav.enabled = false;
+        //}
+    }
+}
